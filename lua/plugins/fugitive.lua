e0bd9f639708f8d732b3952fbd3fd1b762094884
Maciek D <maciek@marketgeni.us>
2025-06-04 23:37:24 -0700

    mason lsp handling, fugitive setup, gitignore DS_Store

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,27 +1,34 @@
 return {
-  "tpope/vim-fugitive",
-  config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    local autocmd = vim.api.nvim_create_autocmd
-    local group = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>gk", vim.cmd.Git)
+        local autocmd = vim.api.nvim_create_autocmd
+        local group = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
 
-    autocmd("BufWinEnter", {
-      group = group,
-      pattern = "*",
-      callback = function()
-        if vim.bo.ft ~= "fugitive" then return end
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { buffer = bufnr, remap = false }
+        autocmd("BufWinEnter", {
+            group = group,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then return end
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = { buffer = bufnr, remap = false }
 
-        vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end, opts)
-        -- rebase always
-        vim.keymap.set("n", "<leader>P", function() vim.cmd.Git({ "pull", "--rebase" }) end, opts)
-        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-        -- needed if i did not set the branch up correctly
-        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+                vim.keymap.set("n", "<leader>gi", function()
+                    vim.cmd("Git add .")
+                end, { noremap = true, silent = true })
 
-      end,
-    })
-  end,
+                vim.keymap.set("n", "<leader>gu", function()
+                    vim.cmd("silent !git reset")
+                    vim.cmd("redraw!")
+                end, { noremap = true, silent = true })
+
+                vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end, opts)
+                -- rebase always
+                vim.keymap.set("n", "<leader>P", function() vim.cmd.Git({ "pull", "--rebase" }) end, opts)
+                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+                -- needed if i did not set the branch up correctly
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+            end,
+        })
+    end,
 }
-
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "pyright",
+          "bashls",
+          "yamlls",
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      mason_lspconfig.setup_handlers {
+        -- Default setup for every server
+        function(server_name)
+          lspconfig[server_name].setup({})
+        end,
+
+        -- Custom setup for Lua
+        ["lua_ls"] = function()
+          lspconfig.lua_ls.setup({
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          })
+        end,
+      }
+    end,
+  },
+}
+
