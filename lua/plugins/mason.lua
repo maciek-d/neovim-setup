1be449e7f815a77f83497540e0606504a0dbcc46
Maciek D <maciek@marketgeni.us>
2025-06-05 11:43:39 -0700

    lsp-zero v3, code suggestions.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,55 +1,90 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "pyright",
-          "bashls",
-          "yamlls",
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
-    config = function()
-      local lspconfig = require("lspconfig")
-      local mason_lspconfig = require("mason-lspconfig")
-
-      mason_lspconfig.setup_handlers {
-        -- Default setup for every server
-        function(server_name)
-          lspconfig[server_name].setup({})
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
         end,
-
-        -- Custom setup for Lua
-        ["lua_ls"] = function()
-          lspconfig.lua_ls.setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "pyright",
+                    "bashls",
+                    "yamlls",
                 },
-              },
-            },
-          })
+            })
         end,
-      }
-    end,
-  },
-}
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "williamboman/mason-lspconfig.nvim",
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
+        },
+        config = function()
+            local lsp_zero = require("lsp-zero")
 
+            local cmp = require("cmp")
+
+            -- extend lsp-zero with default cmp capabilities
+            require("lsp-zero").extend_cmp()
+
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
+                sources = {
+                    { name = 'nvim_lsp' },
+                },
+            })
+
+
+            lsp_zero.on_attach(function(_, bufnr)
+                local opts = { buffer = bufnr, remap = false }
+
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+                vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("n", "<leader>vc", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("i", "<C-g>", vim.lsp.buf.signature_help, opts)
+
+                vim.keymap.set("n", "<leader>K", function()
+                    local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+                    if diagnostics[1] then
+                        vim.fn.setreg("+", diagnostics[1].message)
+                        print("Error copied: " .. diagnostics[1].message)
+                    else
+                        print("No error message on this line.")
+                    end
+                end, { noremap = true, silent = true, buffer = bufnr })
+            end)
+
+            lsp_zero.setup()
+
+            vim.diagnostic.config({ virtual_text = true })
+
+            -- diagnostic signs
+            local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
+        end,
+    },
+}
